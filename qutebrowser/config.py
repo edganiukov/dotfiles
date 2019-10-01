ecc10ed6021725d775211c00a87c757d7886981c
Eduard Ganiukov <ed@gnkv.io>
2019-10-02 00:05:06 +0200

    qutebrowser: color for pinned tabs.
    Signed-off-by: Eduard Ganiukov <ed@gnkv.io>

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -58,27 +58,22 @@ c.auto_save.session= False
 c.spellcheck.languages = ['en-US', 'ru-RU']
 
 # Enable JavaScript.
-# Type: Bool
 config.set('content.javascript.enabled', True, 'file://*')
 
 # Enable JavaScript.
-# Type: Bool
 config.set('content.javascript.enabled', True, 'chrome://*/*')
 
 # Enable JavaScript.
-# Type: Bool
 config.set('content.javascript.enabled', True, 'qute://*/*')
 
 # Enable smooth scrolling for web pages. Note smooth scrolling does not
 # work with the `:scroll-px` command.
-# Type: Bool
 c.scrolling.smooth = False
 
 # Show scrolling bar
 c.scrolling.bar = "always"
 
 # Padding (in pixels) around text for tabs.
-# Type: Padding
 c.tabs.padding = {'bottom': 4, 'left': 4, 'right': 4, 'top': 4}
 
 # Open blank page when close last tab
@@ -95,123 +90,117 @@ c.tabs.max_width = 300
 
 # Page to open if :open -t/-b/-w is used without URL. Use `about:blank`
 # for a blank page.
-# Type: FuzzyUrl
 # c.url.default_page = 'https://ddg.gg'
 c.url.default_page = 'qute:bookmarks'
 
 # Page(s) to open at the start.
-# Type: List of FuzzyUrl, or FuzzyUrl
 # c.url.start_pages = 'https://start.duckduckgo.com/'
 c.url.start_pages = 'qute:bookmarks'
 
-# Hide the window decoration.  This setting requires a restart on
-# Wayland.
-# Type: Bool
+# Hide the window decoration.  This setting requires a restart on Wayland.
 c.window.hide_decoration = False
 
 # Text color of the completion widget. May be a single color to use for
 
 # all columns or a list of three colors, one for each column.
-# Type: List of QtColor, or QtColor
 c.colors.completion.fg = '#d5c4a1'
 
 # Background color of the completion widget for odd rows.
-# Type: QssColor
 c.colors.completion.odd.bg = '#333333'
 
 # Background color of the completion widget for even rows.
-# Type: QssColor
 c.colors.completion.even.bg = '#202020'
 
 # Background color of the selected completion item.
-# Type: QssColor
 c.colors.completion.item.selected.bg = '#8fee96'
 
 # Top border color of the completion widget category headers.
-# Type: QssColor
 c.colors.completion.item.selected.border.top = '#151515'
 
 # Bottom border color of the selected completion item.
-# Type: QssColor
 c.colors.completion.item.selected.border.bottom = '#151515'
 
 # Foreground color of the matched text in the completion.
-# Type: QssColor
 c.colors.completion.match.fg = '#d75f5f'
 
 # Foreground color of the statusbar.
-# Type: QssColor
 c.colors.statusbar.normal.fg = '#d5c4a1'
 
 # Background color of the statusbar.
-# Type: QssColor
 c.colors.statusbar.normal.bg = '#202020'
 
 # Foreground color of the statusbar in command mode.
-# Type: QssColor
 c.colors.statusbar.command.fg = '#d4c5a1'
 
 # Background color of the statusbar in command mode.
-# Type: QssColor
 c.colors.statusbar.command.bg = '#202020'
 
 # Default foreground color of the URL in the statusbar.
-# Type: QssColor
 c.colors.statusbar.url.fg = '#d5c4a1'
 
 # Foreground color of the URL in the statusbar on error.
-# Type: QssColor
 c.colors.statusbar.url.error.fg = '#d75f5f'
 
-# Foreground color of the URL in the statusbar on successful load
-# (http).
-# Type: QssColor
+# Foreground color of the URL in the statusbar on successful load (http).
 c.colors.statusbar.url.success.http.fg = '#84edb9'
 
 # Foreground color of the URL in the statusbar on successful load
 # (https).
-# Type: QssColor
 c.colors.statusbar.url.success.https.fg = '#8fee96'
 
 # Foreground color of the URL in the statusbar when there's a warning.
-# Type: QssColor
 c.colors.statusbar.url.warn.fg = '#cd950c'
 
 # Background color of the tab bar.
-# Type: QtColor
 c.colors.tabs.bar.bg = '#323232'
 
 # Foreground color of unselected odd tabs.
-# Type: QtColor
 c.colors.tabs.odd.fg = '#707070'
 
 # Background color of unselected odd tabs.
-# Type: QtColor
 c.colors.tabs.odd.bg = '#323232'
 
 # Foreground color of unselected even tabs.
-# Type: QtColor
 c.colors.tabs.even.fg = '#707070'
 
 # Background color of unselected even tabs.
-# Type: QtColor
 c.colors.tabs.even.bg = '#323232'
 
 # Foreground color of selected odd tabs.
-# Type: QtColor
 c.colors.tabs.selected.odd.fg = '#d5c4a1'
 
 # Background color of selected odd tabs.
-# Type: QtColor
 c.colors.tabs.selected.odd.bg = '#202020'
 
 # Foreground color of selected even tabs.
-# Type: QtColor
 c.colors.tabs.selected.even.fg = '#d5c4a1'
 
 # Background color of selected even tabs.
-# Type: QtColor
 c.colors.tabs.selected.even.bg = '#202020'
+
+# Foreground color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.fg = '#707070'
+
+# Background color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.bg = '#323232'
+
+# Foreground color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.fg = '#707070'
+
+# Background color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.bg = '#323232'
+
+# Foreground color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.fg = '#d5c4a1'
+
+# Background color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.bg = '#202020'
+
+# Foreground color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.fg = '#d5c4a1'
+
+# Background color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.bg = '#202020'
 
 
 # Fonts
@@ -220,50 +209,39 @@ c.colors.tabs.selected.even.bg = '#202020'
 c.fonts.monospace = '"Source Code Pro"'
 
 # Font used in the completion categories.
-# Type: QtFont
 c.fonts.completion.category = 'bold 9pt monospace'
 
 # Font used in the completion widget.
-# Type: QtFont
 c.fonts.completion.entry = '9pt monospace'
 
 # Font used for the debugging console.
-# Type: QtFont
 c.fonts.debug_console = '9pt monospace'
 
 # Font used for the downloadbar.
-# Type: QtFont
 c.fonts.downloads = '9pt monospace'
 
 # Font used for the hints.
-# Type: QtFont
 c.fonts.hints = 'bold 9pt monospace'
 
 # Font used in the keyhint widget.
-# Type: QtFont
 c.fonts.keyhint = '9pt monospace'
 
 # Font used for error messages.
-# Type: QtFont
 c.fonts.messages.error = '9pt monospace'
 
 # Font used for info messages.
-# Type: QtFont
 c.fonts.messages.info = '9pt monospace'
 
 # Font used for warning messages.
-# Type: QtFont
 c.fonts.messages.warning = '9pt monospace'
 
 # Font used for prompts.
-# Type: QtFont
 c.fonts.prompts = '9pt monospace'
 
 # Font used in the statusbar.
 c.fonts.statusbar = '9pt monospace'
 
 # Font used in the tab bar.
-# Type: QtFont
 c.fonts.tabs = '9pt monospace'
 
 
