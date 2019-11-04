05ebe63f7637d42339d4006ebfab16569148d03a
Eduard Ganiukov <ed@gnkv.io>
2019-11-04 22:14:26 +0100

    i3: update colors.
    Signed-off-by: Eduard Ganiukov <ed@gnkv.io>

--- i3/i3status-xkb-layout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# shell script to prepend i3status with keyboard layout
-
-i3status -c ~/.config/i3/i3status.conf | while :
-do
-    read line
-    echo "$(xkb-switch)  $line" || exit 1
-done
